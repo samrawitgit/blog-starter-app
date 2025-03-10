41ce8065c9da3e61f4d435b0719461c91414b77c
@samrawitgit <samy_tk_18@yahoo.com>
2025-03-10 17:56:55 +0100

    Config ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "no-var": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/display-name": "off"
+    }
+  },
+];
